c0b3bdd084e03a04dd24f76c00ad54e6ff7c3255
fronoske <fronoske@gmail.com>
2022-02-04 02:27:38 +0900

    Set Google Analytics

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -9,7 +9,7 @@ export const CONFIG = {
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
-  googleAnalytics: '', // You can use this if you use Google Analytics
+  googleAnalytics: 'G-WXXNQ6GPSN', // You can use this if you use Google Analytics
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether t6o apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   hint: 0.3,
