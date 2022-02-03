73185662b6fd9680a377db072365e15e7d4a7431
Tsuyoshi Arai <tsuyoshi.arai@sony.com>
2022-02-03 11:13:21 +0900

    Users can select group

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -1,7 +1,9 @@
 import { CONFIG } from './config'
 import { ALL_48NAMES } from './all48names'
 
-export const VALIDGUESSES = ALL_48NAMES[CONFIG.wordLength]
+export const VALIDGUESSES = ALL_48NAMES[CONFIG.wordLength].map((_) =>
+  _.replace(/_[A-Z]{3}48$/, '')
+)
 
 if (CONFIG.normalization) {
   VALIDGUESSES.forEach(
--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,16 +1,20 @@
 import { CONFIG } from './config'
 import { ALL_48NAMES } from './all48names'
 
-export const WORDS = ALL_48NAMES[CONFIG.wordLength].filter((x: string) => {
-  const arr = x.split('')
-  const s = new Set(arr)
-  return (
-    s.size === arr.length &&
-    /^[あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢつづってでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろわをん]+$/.test(
-      x
-    )
-  )
-})
+export const WORDS = ALL_48NAMES[CONFIG.wordLength].filter(
+  (name_with_group: string) => {
+    const word = name_with_group.replace(/_[A-Z]{3}48$/, '')
+    const arr = word.split('')
+    const s = new Set(arr)
+
+    const no_duplicated_chars = s.size === arr.length
+    const all_valid_chars =
+      /^[あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢつづってでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろわをん]+$/.test(
+        word
+      )
+    return no_duplicated_chars && all_valid_chars
+  }
+)
 
 if (CONFIG.normalization) {
   WORDS.forEach((val, i) => (WORDS[i] = val.normalize(CONFIG.normalization)))
