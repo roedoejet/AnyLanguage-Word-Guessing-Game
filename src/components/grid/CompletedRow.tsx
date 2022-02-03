6665514328e28c8f2d79b8750d2adcb1bfce8983
fronoske <fronoske@gmail.com>
2022-02-04 02:33:22 +0900

    Give up animation

--- src/components/grid/CompletedRow.tsx
+++ src/components/grid/CompletedRow.tsx
@@ -1,6 +1,5 @@
 import { getGuessStatuses } from '../../lib/statuses'
 import { Cell } from './Cell'
-import { useEffect } from 'react'
 
 type Props = {
   guess: string[]
@@ -8,21 +7,7 @@ type Props = {
 
 export const CompletedRow = ({ guess }: Props) => {
   const statuses = getGuessStatuses(guess)
-  /*
-  useEffect(() => {
-    // Show completed row cell-by-cell
-    const cellShowDelay = 300
-    const cellsContainer = document.querySelectorAll('.completed:not(.displayed)') ?? []
-    if(cellsContainer[0]){
-      Array.from(cellsContainer[0].children).forEach((cell, i) => {
-        setTimeout(() => {
-            cell.classList.add('shown')
-        }, cellShowDelay*i)
-      });
-      cellsContainer[0].classList.add('displayed')
-    }
-  })
-*/
+
   return (
     <div className="flex justify-center mb-1 completed">
       {guess.map((letter, i) => (
