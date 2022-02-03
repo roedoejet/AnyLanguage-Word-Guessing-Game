c11ea481e94aa487a6a6f7688f38855edef74fed
fronoske <fronoske@gmail.com>
2022-02-04 02:37:09 +0900

    Give up animation

--- src/components/grid/CompletedRow.tsx
+++ src/components/grid/CompletedRow.tsx
@@ -9,7 +9,7 @@ export const CompletedRow = ({ guess }: Props) => {
   const statuses = getGuessStatuses(guess)
 
   return (
-    <div className="flex justify-center mb-1 completed">
+    <div className="flex justify-center mb-1">
       {guess.map((letter, i) => (
         <Cell key={i} value={letter} status={statuses[i]} />
       ))}
