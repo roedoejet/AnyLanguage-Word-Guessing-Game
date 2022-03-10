d3881cbbadfb6ae6633c395d5cec4ded7a263c87
Haowen Jiang <howard.haowen@gmail.com>
2022-03-10 17:33:18 +0800

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -13,6 +13,6 @@ export const CONFIG = {
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'March 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'en', // the default interface language
+  defaultLang: '', // the default interface language
   availableLangs: ['en', 'zh'], // the options available to the user for translation languages
 }
