fee8e22386c2e81685f8b6d04e5ea42f932bd0f2
lbalbuenag <lbalbuena7@gmail.com>
2022-10-12 21:55:03 -0500

    Adaptación a ayujk

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,9 +1,9 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Ayuujk', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
+  author: 'lbalbuenag', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'http://ayuujkjaaky.tlahuitoltepec.com/', // Put a link to your website or social media here
   wordListSource: 'YourDictionarySource', // Describe the source material for your words here
   wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
   //
@@ -13,7 +13,7 @@ export const CONFIG = {
   shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'en', // the default interface language
+  defaultLang: 'es', // the default interface language
   availableLangs: ['en', 'es', 'sw', 'zh'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
