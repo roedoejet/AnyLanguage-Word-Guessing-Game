5b8598102e369e30d156958a32fa6e2d4a6514c5
dzakyem <jacmoun@gmail.com>
2023-11-08 13:17:28 +0100

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,10 +2,10 @@ export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'YourLanguageHere', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author: 'Dzakye', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'https://patwe.ch', // Put a link to your website or social media here
+  wordListSource: 'Dichyónér.ó dou patwé dé Chavyeje. ', // Describe the source material for your words here
+  wordListSourceLink: 'https://patwe.ch/lexique-favre-balet/recherche-lexique-balet-favre/', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
